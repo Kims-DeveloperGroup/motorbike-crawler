d027cc7c10955011c6bcb1505ba99f756df9a645
rikim <rikim@riotgames.com>
2017-07-15 23:04:36 +0900

    Delete CrawlTask.class Simply HttpFetcher will do fetching task by calling get(url) That supports multi-threading

--- src/test/java/com/devoo/kim/HttpFetcherTest.java
+++ src/test/java/com/devoo/kim/HttpFetcherTest.java
@@ -1,19 +1,26 @@
 package com.devoo.kim;
 
+import com.devoo.kim.config.HttpFetcherConfig;
 import crawlercommons.fetcher.BaseFetchException;
 import crawlercommons.fetcher.FetchedResult;
+import crawlercommons.fetcher.http.BaseHttpFetcher;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by rikim on 2017. 7. 15..
  */
-@SpringBootTest
-public class CrawlTaskTest {
-
+@RunWith(SpringRunner.class)
+@SpringBootTest (classes = {HttpFetcherConfig.class})
+public class HttpFetcherTest {
+    @Autowired
+    BaseHttpFetcher httpFetcher;
     @Test
     public void shouldCrawlTaskGetDocumentFromWebsite() throws BaseFetchException {
         String targetUrl;
@@ -22,8 +29,7 @@ public class CrawlTaskTest {
         }
         FetchedResult result;
         WHEN: {
-            CrawlTask crawlTask = new CrawlTask(targetUrl);
-            result = crawlTask.crawl();
+            result = httpFetcher.get(targetUrl);
         }
 
         THEN: {
