aa00883ffcb268a52a4a5cbe920fb0147fe72028
rikim <rikim@riotgames.com>
2017-07-16 18:37:05 +0900

    Update .gitignore excluding build/ gradle/

---
+++ src/main/java/com/devoo/kim/storage/FetchedResultStorage.java
@@ -0,0 +1,12 @@
+package com.devoo.kim.storage;
+
+import crawlercommons.fetcher.FetchedResult;
+
+import java.util.List;
+
+/**
+ * Created by rikim on 2017. 7. 16..
+ */
+public interface FetchedResultStorage {
+    void save();
+}
