26ae7e25d19cb10130e6f20a76c0882049b28b00
rikim <rikim@riotgames.com>
2017-07-17 00:43:33 +0900

    ADD NaverCafeAPI to search documents posted in naver cafe

---
+++ src/main/java/com/devoo/kim/external/api/naver/NaverCafeAPI.java
@@ -0,0 +1,11 @@
+package com.devoo.kim.external.api.naver;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriTemplate;
+
+/**
+ * Created by rikim on 2017. 7. 16..
+ */
+@Service
+public class NaverCafeAPI {
+}
