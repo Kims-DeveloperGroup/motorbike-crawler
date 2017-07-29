4397f546b25d866ee65380aa77894762252333cf
rikim <rikim@riotgames.com>
2017-07-30 00:39:42 +0900

    Add data-template-1 from pass..

---
+++ src/main/java/com/devoo/kim/util/DomManipulator.java
@@ -0,0 +1,7 @@
+package com.devoo.kim.util;
+
+/**
+ * Created by rikim on 2017. 7. 17..
+ */
+public class DomManipulator {
+}
---
+++ src/test/java/com/devoo/kim/util/DomManipulatorTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by rikim on 2017. 7. 17..
+ */
+public class DomManipulatorTest {
+
+}
