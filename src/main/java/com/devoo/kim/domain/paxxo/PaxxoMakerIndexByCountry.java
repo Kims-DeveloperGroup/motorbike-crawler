3370a6fbd6915ddba0be147431dd7bf0e8db8a84
rikim <rikim@riotgames.com>
2017-08-22 00:03:19 -0700

    Add getMakers() in PaxxoMakerIndices

--- src/main/java/com/devoo/kim/domain/paxxo/PaxxoMakerIndexByCountry.java
+++ src/main/java/com/devoo/kim/domain/paxxo/PaxxoMakerIndexByCountry.java
@@ -1,5 +1,7 @@
 package com.devoo.kim.domain.paxxo;
 
+import lombok.Getter;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -7,6 +9,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Created by rikim on 2017. 8. 14..
  */
 @XmlRootElement(name = "country")
+@Getter
 public class PaxxoMakerIndexByCountry {
     @XmlElement(name = "maker")
     private Maker[] makers;
--- src/main/java/com/devoo/kim/domain/paxxo/PaxxoMakerIndices.java
+++ src/main/java/com/devoo/kim/domain/paxxo/PaxxoMakerIndices.java
@@ -2,6 +2,9 @@ package com.devoo.kim.domain.paxxo;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by rikim on 2017. 8. 10..
@@ -10,4 +13,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class PaxxoMakerIndices {
     @XmlElement(name = "country")
     private PaxxoMakerIndexByCountry[] makerIndexByCountries;
+
+    public List<Maker> getMakers() {
+        List<Maker> makers = new ArrayList<>();
+        for (PaxxoMakerIndexByCountry country: makerIndexByCountries) {
+            makers.addAll(Arrays.asList(country.getMakers()));
+        }
+        return makers;
+    }
 }
