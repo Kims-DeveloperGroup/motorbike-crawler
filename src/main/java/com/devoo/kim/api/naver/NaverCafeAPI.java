1197f9ce9af5ad24fe7c528cbbd1dd2710025c8e
rikim <rikim@riotgames.com>
2017-09-10 22:26:36 +0900

    Add doc in NaverCafeApi

--- src/main/java/com/devoo/kim/api/naver/NaverCafeAPI.java
+++ src/main/java/com/devoo/kim/api/naver/NaverCafeAPI.java
@@ -30,10 +30,24 @@ public class NaverCafeAPI {
     private RestTemplate restTemplate = new RestTemplate();
     private String SIMILARITY_ORDER = "sim";
 
+    /**
+     * Search items in Naver cafe
+     * @param query
+     * @param countOfResult
+     * @return
+     */
     public ResponseEntity<String> search(String query, int countOfResult) {
         return doRequest(query, SIMILARITY_ORDER, countOfResult, 1);
     }
 
+    /**
+     * Send a requests to search api of Naver cafe
+     * @param query query input to search
+     * @param sort sorting method, order
+     * @param countOfResult limit count of items from search result
+     * @param start start index of items from result.
+     * @return
+     */
     private ResponseEntity<String> doRequest(String query, String sort, int countOfResult, int start) {
         URI url = UriComponentsBuilder.fromHttpUrl(API_URL)
                 .queryParam("query", query)
@@ -44,6 +58,10 @@ public class NaverCafeAPI {
         return response;
     }
 
+    /**
+     * creates request header with authentication.
+     * @return http entities with auth
+     */
     private HttpEntity<Void> header() {
         MultiValueMap<String, String> auth = new LinkedMultiValueMap<>();
         auth.add(CLIENT_ID_HEADER, clientId);
