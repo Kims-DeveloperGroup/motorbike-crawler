9e1dcc96338e6a54404157f4f1e0bd0adbf7c0de
rikim <rikim@riotgames.com>
2017-09-10 23:43:55 +0900

    Add field totalItem to SearchMetadata

--- src/main/java/com/devoo/kim/domain/naver/SearchMetadata.java
+++ src/main/java/com/devoo/kim/domain/naver/SearchMetadata.java
@@ -1,4 +1,11 @@
 package com.devoo.kim.domain.naver;
 
+import lombok.Data;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+@Data
 public class SearchMetadata {
+
+    @JsonProperty("total")
+    private long totalItems;
 }
