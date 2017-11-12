88d5ce0fd4988db097663c00c4f0ce8d838f07b6
rikim <rikim@riotgames.com>
2017-11-12 22:16:07 +0900

    Add @Repository annotation to EsRepository interface

--- src/main/java/com/devoo/kim/repository/paxxo/PaxxoMakerIndexRepository.java
+++ src/main/java/com/devoo/kim/repository/paxxo/PaxxoMakerIndexRepository.java
@@ -2,8 +2,10 @@ package com.devoo.kim.repository.paxxo;
 
 import com.devoo.kim.domain.paxxo.Maker;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
+@Repository
 public interface PaxxoMakerIndexRepository extends ElasticsearchRepository<Collection<Maker>, Long> {
 }
