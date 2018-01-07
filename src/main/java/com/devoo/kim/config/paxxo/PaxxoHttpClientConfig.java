b4045e1746b59413029a4e898dbdc6c66c2a04f5
rikim <rikim@riotgames.com>
2018-01-07 15:00:05 +0900

    Extract values of paxxoHttpClientRequestFactory to application.yml

--- src/main/java/com/devoo/kim/config/paxxo/PaxxoHttpClientConfig.java
+++ src/main/java/com/devoo/kim/config/paxxo/PaxxoHttpClientConfig.java
@@ -2,6 +2,7 @@ package com.devoo.kim.config.paxxo;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -11,15 +12,27 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class PaxxoHttpClientConfig {
 
+    @Value("${external.paxxo.httpclient.connectionTimeout}")
     private final int connectionTimeoutInMills = 10 * 1000;
+
+    @Value("${external.paxxo.httpclient.readTimeout}")
     private final int readTimeoutInMills = connectionTimeoutInMills * 2;
+
+    @Value("${external.paxxo.httpclient.connectionTimeToLive}")
+    private final int connectionTimeToLive = 100000;
+
+    @Value("${external.paxxo.httpclient.maxTotalConnection}")
+    private final int maxTotalConnection = 16;
+
+    @Value("${external.paxxo.httpclient.maxConnectionPerRoute}")
+    private final int maxConnectionPerRoute = 8;
 
     @Bean
     public HttpComponentsClientHttpRequestFactory paxxoHttpClientRequestFactory() {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setMaxConnTotal(16)
-                .setMaxConnPerRoute(8)
-                .setConnectionTimeToLive(1, TimeUnit.MINUTES)
+                .setMaxConnTotal(maxTotalConnection)
+                .setMaxConnPerRoute(maxConnectionPerRoute)
+                .setConnectionTimeToLive(connectionTimeToLive, TimeUnit.MILLISECONDS)
                 .build();
         HttpComponentsClientHttpRequestFactory httpClientRequestFactory = new HttpComponentsClientHttpRequestFactory();
         httpClientRequestFactory.setHttpClient(httpClient);
