b32252f5ee56976f69fa0bf9d3b48c34ddae842a
rikim <rikim@riotgames.com>
2018-02-04 20:07:31 +0900

    Modify NaverQueryCreator.getQueries() by adding readModelsFromPaxxo() , which reads model names from paxxoRepo and add as query

--- src/main/java/com/devoo/kim/query/NaverQueryCreator.java
+++ src/main/java/com/devoo/kim/query/NaverQueryCreator.java
@@ -1,25 +1,45 @@
 package com.devoo.kim.query;
 
+import com.devoo.kim.repository.paxxo.PaxxoModelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class NaverQueryCreator {
 
+    private PaxxoModelRepository modelRepository;
+
+    @Autowired
+    public NaverQueryCreator(PaxxoModelRepository modelRepository) {
+        this.modelRepository = modelRepository;
+    }
+
     private final Path QUERY_FILE_PATH = Paths.get("naver.query");
 
 
     public List<String> getQueries() throws IOException {
-        return readWordsFromQueryFile();
+        List<String> queries = readWordsFromQueryFile();
+        queries.addAll(readModelsFromPaxxo());
+        return queries;
     }
 
     private List<String> readWordsFromQueryFile() throws IOException {
         List<String> queries = Files.readAllLines(QUERY_FILE_PATH);
         return queries;
+    }
+
+    private List<String> readModelsFromPaxxo() {
+        List<String> names = new LinkedList<>();
+        modelRepository.findAll().forEach(paxxoModel -> {
+            names.add(paxxoModel.getName());
+        });
+        return names;
     }
 }
